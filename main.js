2d05396b7176772abc04f6134b6aac30d694f991
bbexpress <bbexpress@gmail.com>
2021-11-12 13:48:58 -0600

    added logic

---
+++ main.js
@@ -0,0 +1,81 @@
+let count = 0;
+const xImage = '<img src="/img/x.png" width="100px" height="100px">';
+const oImage = '<img src="/img/o.png" width="100px" height="100px">';
+function ticMark(position){
+
+    console.log("position " + position);
+    console.log(document.getElementById(position).innerHTML);
+
+    if(document.getElementById(position).innerHTML == '<img src="/img/square.png" width="100px" height="100px">'){
+        if(count % 2 === 0){
+            document.getElementById(position).innerHTML = xImage;
+            if(winCheck(xImage)){
+                alert('X won');
+            }
+            document.getElementById("turn").innerHTML = "O turn";
+        }else{
+            document.getElementById(position).innerHTML = oImage;
+            if(winCheck(oImage)){
+                alert('O won');
+            }
+            document.getElementById("turn").innerHTML = "X turn";
+        }
+
+        //GAME LOGIC
+
+        count++;
+    }else{
+        alert("Pick an open spot!");
+    }
+
+}
+
+function winCheck(turn){
+
+
+console.log("in win check " + document.getElementById("1").innerHTML);
+console.log(turn);
+
+    if((document.getElementById("1").innerHTML === document.getElementById("2").innerHTML && 
+       document.getElementById("2").innerHTML === document.getElementById("3").innerHTML &&
+       document.getElementById("3").innerHTML === turn) 
+       ||
+       (document.getElementById("4").innerHTML === document.getElementById("5").innerHTML && 
+       document.getElementById("5").innerHTML === document.getElementById("6").innerHTML &&
+       document.getElementById("6").innerHTML === turn)
+       ||
+       (document.getElementById("7").innerHTML === document.getElementById("8").innerHTML && 
+       document.getElementById("8").innerHTML === document.getElementById("9").innerHTML &&
+       document.getElementById("9").innerHTML === turn)
+       ||
+       (document.getElementById("1").innerHTML === document.getElementById("4").innerHTML && 
+       document.getElementById("4").innerHTML === document.getElementById("7").innerHTML &&
+       document.getElementById("7").innerHTML === turn)
+       ||
+       (document.getElementById("2").innerHTML === document.getElementById("5").innerHTML && 
+       document.getElementById("5").innerHTML === document.getElementById("8").innerHTML &&
+       document.getElementById("8").innerHTML === turn)
+       ||
+       (document.getElementById("3").innerHTML === document.getElementById("6").innerHTML && 
+       document.getElementById("6").innerHTML === document.getElementById("9").innerHTML &&
+       document.getElementById("9").innerHTML === turn)
+       ||
+       (document.getElementById("7").innerHTML === document.getElementById("8").innerHTML && 
+       document.getElementById("8").innerHTML === document.getElementById("9").innerHTML &&
+       document.getElementById("9").innerHTML === turn)
+       ||
+       (document.getElementById("1").innerHTML === document.getElementById("5").innerHTML && 
+       document.getElementById("5").innerHTML === document.getElementById("9").innerHTML &&
+       document.getElementById("9").innerHTML === turn)
+       ||
+       (document.getElementById("3").innerHTML === document.getElementById("5").innerHTML && 
+       document.getElementById("5").innerHTML === document.getElementById("7").innerHTML &&
+       document.getElementById("7").innerHTML === turn)
+       ){
+        return true;
+    }else{
+        return false;
+    }
+
+
+}
